a74350386d81f767a89a2ace7c405dbabd814389
c0dingfun <hackerrank888@gmail.com>
2020-03-15 18:19:34 -0700

    Added new Roles - Admin/Executive

--- BikeExchange/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BikeExchange/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel;
 
 namespace BikeExchange.Areas.Identity.Pages.Account
 {
@@ -42,9 +43,16 @@ namespace BikeExchange.Areas.Identity.Pages.Account
 
         public class InputModel
         {
+            //[Required]
+            //[EmailAddress]
+            //public string Email { get; set; }
+
+            // Use UserName to login instead
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+
+            //[DisplayName("User Name")]    Use [Display] 
+            [Display(Name = "User Name")]
+            public string UserName { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
@@ -79,7 +87,8 @@ namespace BikeExchange.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                //var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
