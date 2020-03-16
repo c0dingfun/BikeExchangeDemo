cbf630f7427c2eccda201396af6d2687701d51fc
c0dingfun <hackerrank888@gmail.com>
2020-03-15 19:22:13 -0700

    more auth related changes

--- BikeExchange/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BikeExchange/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -17,7 +17,8 @@ using Microsoft.Extensions.Logging;
 
 namespace BikeExchange.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
+    //[AllowAnonymous]
+    [Authorize(Roles ="Admin")]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
@@ -129,15 +130,17 @@ namespace BikeExchange.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+                    //if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    //{
+                    //    return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
+                    //}
+                    //else
+                    //{
+                    //    await _signInManager.SignInAsync(user, isPersistent: false);
+                    //    return LocalRedirect(returnUrl);
+                    //}
+
+                    return RedirectToAction("Index");
                 }
                 foreach (var error in result.Errors)
                 {
